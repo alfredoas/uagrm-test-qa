104e266c13d2674fda9f3a5b1905047c60bb0f4d
alfredoavilasoto <alfredoavilasoto@gmail.com>
2018-04-10 20:59:43 -0400

    se refactorizo el metodo isPrimo

--- urbanager/backend/urbanager-application/src/main/java/com/alenasoft/urbanager/resources/primo/PrimoService.java
+++ urbanager/backend/urbanager-application/src/main/java/com/alenasoft/urbanager/resources/primo/PrimoService.java
@@ -3,7 +3,7 @@ package com.alenasoft.urbanager.resources.primo;
 public class PrimoService {
 
     public String isPrimo(int value){
-    String result = "";
+    String result = "FAIL";
     int j =0;
         for (int i = 2; i<value ; i++){
             if(value % i ==0){
@@ -12,8 +12,6 @@ public class PrimoService {
         }
         if (j==0){
             result = "OK";
-        }else{
-            result = "FAIL";
         }
         return result;
     }
