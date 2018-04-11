82cfed9c62882174264317a2e21fd47ab4c34072
alfredoavilasoto <alfredoavilasoto@gmail.com>
2018-04-10 20:54:02 -0400

    se codificó el metodo isPrimo y pasó la prueba

--- urbanager/backend/urbanager-application/src/main/java/com/alenasoft/urbanager/resources/primo/PrimoService.java
+++ urbanager/backend/urbanager-application/src/main/java/com/alenasoft/urbanager/resources/primo/PrimoService.java
@@ -1,4 +1,18 @@
 package com.alenasoft.urbanager.resources.primo;
 
 public class PrimoService {
+
+    public String isPrimo(int value){
+    String result = "";
+    int j =0;
+        for (int i = 1; i<=value ; i++){
+            if(value % i ==0){
+                j++;
+            }
+        }
+        if (j==2){
+            result = "OK";
+        }
+        return result;
+    }
 }
