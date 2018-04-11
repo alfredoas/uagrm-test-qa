034ac1cc0a1b45afad1b6a6a904ee298c8ff43df
alfredoavilasoto <alfredoavilasoto@gmail.com>
2018-04-10 20:43:49 -0400

    no se refactorizó el metodo isPrimo

--- urbanager/backend/urbanager-application/src/test/java/com/alenasoft/urbanager/resources/primo/PrimoServiceTest.java
+++ urbanager/backend/urbanager-application/src/test/java/com/alenasoft/urbanager/resources/primo/PrimoServiceTest.java
@@ -8,9 +8,17 @@ import static org.junit.Assert.*;
 public class PrimoServiceTest {
 
     @Test
-    public void testIsPrimoIfIntegerIsPrimoShouldReturnOK() {
+    public void testIsPrimoIfIntegerPrimoIsPrimoShouldReturnOK() {
         PrimoService primoService = new PrimoService();
         int value = 5;
+        String expected = "OK";
+        assertEquals(expected, primoService.isPrimo(value));
+    }
+
+    @Test
+    public void testIsPrimoIfIntergerNotPrimoIsPrimoShouldReturnFAIL() {
+        PrimoService primoService = new PrimoService();
+        int value = 1;
         String expected = "OK";
         assertEquals(expected, primoService.isPrimo(value));
     }
