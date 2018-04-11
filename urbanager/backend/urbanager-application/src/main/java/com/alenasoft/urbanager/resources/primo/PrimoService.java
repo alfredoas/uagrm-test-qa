0a03839034ef21cf082d39b5bbd243616ec74be6
alfredoavilasoto <alfredoavilasoto@gmail.com>
2018-04-10 21:11:28 -0400

    se refactorizó la clase  PrimoService para separarlo en una interfaz y una implementacion

--- urbanager/backend/urbanager-application/src/main/java/com/alenasoft/urbanager/resources/primo/PrimoService.java
+++ urbanager/backend/urbanager-application/src/main/java/com/alenasoft/urbanager/resources/primo/PrimoService.java
@@ -1,21 +1,7 @@
 package com.alenasoft.urbanager.resources.primo;
 
-public class PrimoService {
+public interface PrimoService {
 
-    public String isPrimo(int value) throws Exception {
-    String result = "FAIL";
-    if (value < 1) {
-        throw new Exception();
-    }
-    int j =0;
-        for (int i = 2; i<value ; i++){
-            if(value % i ==0){
-                j++;
-            }
-        }
-        if (j==0){
-            result = "OK";
-        }
-        return result;
-    }
+    public String isPrimo(int value) throws Exception;
+
 }
